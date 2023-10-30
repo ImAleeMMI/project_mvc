a8658aa73afd5b343b9f709394b2c20ad1135fb1
ImAleeMMI <147595619+ImAleeMMI@users.noreply.github.com>
2023-10-30 17:18:49 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+// Load Config
+require_once '../config/config.php';
+require_once '../vendor/autoload.php';
+// Autoloader
+require_once '../vendor/autoload.php';
+
+// Load Config
+require_once '../config/config.php';
+
+// Routes
+require_once '../routes/web.php';
+require_once '../app/Router.php';
+?>
